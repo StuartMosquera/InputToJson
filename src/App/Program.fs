3ea97b03205932ae7812c11ba3d357b8bc2c663a
Stuart Mosquera <stuartmosquera18@gmail.com>
2025-09-11 12:36:57 -0300

    fix indentation

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -2,8 +2,8 @@
 open Library
 
 [<EntryPoint>]
-    let main args = 
-        printfn "Nice command-line arguments! Here's what System.Text.Json has to say about them:"
+let main args = 
+    printfn "Nice command-line arguments! Here's what System.Text.Json has to say about them:"
     
     let value, json = getJson {| args = args; year = DateTime.Now.Year |}
 
