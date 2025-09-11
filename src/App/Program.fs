4e48c5b0da44ba62e103267c9e5650b8a1002b1c
Stuart Mosquera <stuartmosquera18@gmail.com>
2025-09-11 12:31:58 -0300

    create the console app

---
+++ src/App/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+open Library
+
+[<EntryPoint>]
+    let main args = 
+        printfn "Nice command-line arguments! Here's what System.Text.Json has to say about them:"
+    
+    let value, json = getJson {| args = args; year = DateTime.Now.Year |}
+
+    printfn $"Input: %0A{value}"
+    printfn $"Output: %s{json}"
+
+    0 // Return an integer exit code
